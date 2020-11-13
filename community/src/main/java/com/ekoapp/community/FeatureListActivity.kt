0f75b63689f186cf06f086bb73faee239e941003
Farhana Haque <fhaque.cse05@gmail.com>
2020-11-13 16:48:14 +0600

    add features

--- community/src/main/java/com/ekoapp/community/FeatureListActivity.kt
+++ community/src/main/java/com/ekoapp/community/FeatureListActivity.kt
@@ -4,7 +4,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ekoapp.community.homepage.MyCustomCommunityHomePageActivity
+import com.ekoapp.community.newsfeed.CreatePostActivity
+import com.ekoapp.community.newsfeed.NewsFeedHomeActivity
 import com.ekoapp.ekosdk.uikit.community.home.activity.EkoCommunityHomePageActivity
+import com.ekoapp.ekosdk.uikit.community.ui.view.EkoCommunityCreateActivity
 import kotlinx.android.synthetic.main.activity_feature_list.*
 
 class FeatureListActivity : AppCompatActivity() {
@@ -19,6 +22,21 @@ class FeatureListActivity : AppCompatActivity() {
 
         btnOpenCustomCommunity.setOnClickListener{
             val intent = Intent(this, MyCustomCommunityHomePageActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnCreateCommunity.setOnClickListener{
+            val intent = Intent(this, EkoCommunityCreateActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnCommunityHome.setOnClickListener{
+            val intent = Intent(this, NewsFeedHomeActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnCreatePost.setOnClickListener {
+            val intent = Intent(this, CreatePostActivity::class.java)
             startActivity(intent)
         }
     }
