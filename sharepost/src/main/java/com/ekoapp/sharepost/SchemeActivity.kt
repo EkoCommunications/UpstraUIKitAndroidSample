223346eac460cb46295f9bf7b880ebc2b325d0dc
Chattip Soontaku <chattip@ekoapp.com>
2021-01-28 18:51:55 +0700

    Open default community page

--- sharepost/src/main/java/com/ekoapp/sharepost/SchemeActivity.kt
+++ sharepost/src/main/java/com/ekoapp/sharepost/SchemeActivity.kt
@@ -5,16 +5,18 @@ import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.ekoapp.ekosdk.uikit.community.home.activity.EkoCommunityHomePageActivity
+import kotlinx.android.synthetic.main.activity_feature_list.*
 
 class SchemeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_feature_list)
 
-        val intent = Intent(this, EkoCommunityHomePageActivity::class.java)
-        startActivity(intent)
-
-        finish()
+        btnOpenDefaultCommunity.setOnClickListener {
+            val intent = Intent(this, EkoCommunityHomePageActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     private fun handleDeepLink(callback: (Uri) -> Unit) {
